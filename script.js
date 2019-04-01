27d53e63aa3ab49b5f157c1c8a1fbb365ecf5d2e
Khistunning <43241730+Khistunning@users.noreply.github.com>
2019-04-01 09:34:12 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,16 @@
+$("button").click(function() {
+var Age =parseInt( $(".Age").val());    
+var color= $(".color").val();
+
+if (color==="Green" && age >19){ 
+    $(".answer").text("you are lil durk");
+}else if(color==="red" && Age <19){
+    $(".answer").text("you are meek mill");
+    
+    
+
+
+}
+    
+});
+
